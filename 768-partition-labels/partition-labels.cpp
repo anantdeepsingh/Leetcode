79eeac614f5114bb5c6b4ba9f5145e5c2c7b070b
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-03-30 19:12:48 +0530

    Time: 1 ms (47.01%) | Memory: 9 MB (49.45%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        map<char,int>last;
+        unordered_map<char,int>last;
         int n=s.size();
         for(int i=0;i<n;i++){
             last[s[i]]=i;
         }
 
         int ind=-1;
-        map<char,int>mp;
         vector<int>ans;
         int end=-1;
         for(int i=0;i<n;i++){
