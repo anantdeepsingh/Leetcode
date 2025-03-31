1819f1d3e5375f1f3802e82edebc36e9de225d24
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-04-01 01:13:14 +0530

    Time: 45 ms (25.85%) | Memory: 73.3 MB (14.41%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -1,20 +1,31 @@
+#define ll long long
 class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
-        if (k == 1) {
-            return 0;
+        vector<ll>v;
+        int n=weights.size();
+        for(int i=1;i<n;i++){
+            v.push_back(1ll*weights[i]+1ll*weights[i-1]);
+        }
+        ll temp=k-1;
+        sort(v.begin(),v.end());
+        ll mini=0;
+        int ind=0;
+        while(temp>0){
+            mini+=v[ind];
+            ind++;
+            temp--;
+        }
+        sort(v.begin(),v.end(),greater<int>());
+        ll maxi=0;
+        ind=0;
+        temp=k-1;
+        while(temp>0){
+            maxi+=v[ind];
+            ind++;
+            temp--;
         }
 
-        vector<int> pair_sums;
-        for (size_t i = 0; i < weights.size() - 1; ++i) {
-            pair_sums.push_back(weights[i] + weights[i + 1]);
-        }
-
-        sort(pair_sums.begin(), pair_sums.end());
-
-        long long min_score = accumulate(pair_sums.begin(), pair_sums.begin() + (k - 1), 0LL);
-        long long max_score = accumulate(pair_sums.end() - (k - 1), pair_sums.end(), 0LL);
-
-        return max_score - min_score;
+        return maxi-mini;
     }
 };
