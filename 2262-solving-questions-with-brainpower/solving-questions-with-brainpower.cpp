2d54e5e87129ea0f7673e24ffb91a22e4026d42b
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-04-01 23:04:12 +0530

    Time: 12 ms (28.33%) | Memory: 123 MB (51.86%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -2,17 +2,19 @@
 class Solution {
 public:
     int n;
-    ll solve(int ind,vector<vector<int>>& questions,vector<ll>&dp){
-        if(ind>=n) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        ll npick=solve(ind+1,questions,dp);
-        ll pick=0;
-        pick=1ll*questions[ind][0]+1ll*solve(ind+questions[ind][1]+1,questions,dp);
-        return dp[ind]=max(1ll*npick,1ll*pick);
+    ll dp[100005];  // Increased size to avoid out-of-bounds errors
+    
+    ll solve(int ind, vector<vector<int>>& questions) {
+        if (ind >= n) return 0;
+        if (dp[ind] != -1) return dp[ind];
+        ll npick = solve(ind + 1, questions);
+        ll pick = 1ll * questions[ind][0] + solve(ind + questions[ind][1] + 1, questions);
+        return dp[ind] = max(npick, pick);
     }
+
     long long mostPoints(vector<vector<int>>& questions) {
-        n=questions.size();
-        vector<ll>dp(n+1,-1ll);
-        return solve(0,questions,dp);
+        n = questions.size();
+        memset(dp, -1, sizeof(dp)); 
+        return solve(0, questions);
     }
 };
