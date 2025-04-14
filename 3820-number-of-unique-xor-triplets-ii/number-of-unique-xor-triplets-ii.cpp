7638f4fac27c5a32252ec2d7bb9e456f268e5517
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-04-14 20:17:39 +0530

    Time: 511 ms (77.14%) | Memory: 113.3 MB (71.43%) - LeetSync

---
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n=nums.size();
+        cout<<n<<endl;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        vector<int>v1(10000,0),v2(10000,0);
+        map<int,int>mp;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int val=nums[i]^nums[j];
+                v1[val]=1;
+            }
+        }  
+        for(int i=0;i<=2048;i++){
+            for(int j=0;j<n;j++){
+                int val=nums[j]^i;
+                if(v1[val]){
+                    v2[i]=1;
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<=2048;i++){
+            if(v2[i]==1){
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
