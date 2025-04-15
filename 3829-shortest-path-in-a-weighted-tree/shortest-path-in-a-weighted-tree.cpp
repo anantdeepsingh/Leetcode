956728e5f22a95a200192a8a7e226c59a981cdda
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-04-15 12:04:02 +0530

    Time: 1132 ms (26.14%) | Memory: 474.5 MB (5.04%) - LeetSync

--- 3829-shortest-path-in-a-weighted-tree/shortest-path-in-a-weighted-tree.cpp
+++ 3829-shortest-path-in-a-weighted-tree/shortest-path-in-a-weighted-tree.cpp
@@ -1,77 +1,134 @@
-vector<array<int,2>> a[100005];
-long long seg[800005],lazy[800005],st[100005],ed[100005],sum[100005];
-int timer;
+#define pi pair<long long,int>
 
-void euler(int node,int par=-1){
-    st[node]=timer++;
-    for(auto edge: a[node]){
-        if(edge[0]==par) continue;
-        sum[edge[0]]=sum[node]+edge[1];
-        euler(edge[0],node);
+class ST {
+public:
+    vector<long long> st, lazy;
+
+    ST(int n) {
+        st.resize(4 * n);
+        lazy.resize(4 * n);
     }
-    ed[node]=timer++;
-}
 
-void push(int id, int l, int r) {
-    if (lazy[id] != 0) {
-        seg[id] += (r - l + 1) * lazy[id];
-        if (l != r) {
-            lazy[id * 2] += lazy[id];
-            lazy[id * 2 + 1] += lazy[id];
+    void build(int ind, int low, int high, vector<long long>& arr) {
+        if (low == high) {
+            st[ind] = arr[low];
+            return;
         }
-        lazy[id] = 0;
+        int mid = (low + high) / 2;
+        build(2 * ind + 1, low, mid, arr);
+        build(2 * ind + 2, mid + 1, high, arr);
+        st[ind] = st[2 * ind + 1] + st[2 * ind + 2];
     }
-}
 
-void update(int id, int l, int r, int ql, int qr, int val) {
-    push(id, l, r);
-    if (r < ql || l > qr) return;
-    if (ql <= l && r <= qr) {
-        lazy[id] += val;
-        push(id, l, r);
-        return;
+    void push(int ind, int low, int high) {
+        if (lazy[ind] != 0) {
+            st[ind] += (high - low + 1LL) * lazy[ind];
+            if (low != high) {
+                lazy[2 * ind + 1] += lazy[ind];
+                lazy[2 * ind + 2] += lazy[ind];
+            }
+            lazy[ind] = 0;
+        }
     }
-    int mid = (l + r) / 2;
-    update(id * 2, l, mid, ql, qr, val);
-    update(id * 2 + 1, mid + 1, r, ql, qr, val);
-    seg[id] = seg[id * 2] + seg[id * 2 + 1];
-}
 
-long long get(int id, int l, int r, int idx) {
-    push(id, l, r);
-    if (l == r) return seg[id];
-    int mid = (l + r) / 2;
-    if (idx <= mid)
-        return get(id * 2, l, mid, idx);
-    else
-        return get(id * 2 + 1, mid + 1, r, idx);
-}
+    void update(int ind, int low, int high, int l, int r, long long val) {
+        push(ind, low, high);
+        if (high < l || low > r) return;
+        if (low >= l && high <= r) {
+            lazy[ind] += val;
+            push(ind, low, high);
+            return;
+        }
+        int mid = (low + high) / 2;
+        update(2 * ind + 1, low, mid, l, r, val);
+        update(2 * ind + 2, mid + 1, high, l, r, val);
+        st[ind] = st[2 * ind + 1] + st[2 * ind + 2];
+    }
+
+    long long query(int ind, int low, int high, int l, int r) {
+        push(ind, low, high);
+        if (high < l || low > r) return 0;
+        if (low >= l && high <= r) return st[ind];
+        int mid = (low + high) / 2;
+        long long left = query(2 * ind + 1, low, mid, l, r);
+        long long right = query(2 * ind + 2, mid + 1, high, l, r);
+        return left + right;
+    }
+};
 
 class Solution {
 public:
+    void dfs(int node, int par, vector<vector<pair<int, int>>>& adj, vector<int>& euler) {
+        euler.push_back(node);
+        for (auto& it : adj[node]) {
+            int nnode = it.first;
+            if (nnode == par) continue;
+            dfs(nnode, node, adj, euler);
+        }
+        euler.push_back(node);
+    }
+
     vector<int> treeQueries(int n, vector<vector<int>>& edges, vector<vector<int>>& queries) {
-        map<array<int,2>,int> weights;
-        for(int i=0;i<=n;i++){
-            a[i].clear();
-            st[i]=ed[i]=sum[i]=0;
+        map<pair<int, int>, int> mp;
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for (auto& it : edges) {
+            int u = it[0], v = it[1], w = it[2];
+            adj[u].emplace_back(v, w);
+            adj[v].emplace_back(u, w);
+            mp[{u, v}] = w;
+            mp[{v, u}] = w;
         }
-        for(int i=0;i<=8*n;i++) seg[i]=lazy[i]=0;
-        for(auto u: edges){
-            weights[{u[0],u[1]}]=weights[{u[1],u[0]}]=u[2];
-            a[u[0]].push_back({u[1],u[2]});
-            a[u[1]].push_back({u[0],u[2]});
-        }
-        timer=1;
-        euler(1);
-        vector<int> ans;
-        for(auto query: queries){
-            if(query[0]==1){
-                int u=query[1],v=query[2];
-                if(st[u]>st[v]) swap(u,v);
-                update(1,0,2*n,st[v],ed[v],query[3]-weights[{u,v}]);
-                weights[{u,v}]=weights[{v,u}]=query[3];
+
+        vector<long long> dist(n + 1, 1e18);
+        priority_queue<pi, vector<pi>, greater<pi>> pq;
+        pq.push({0, 1});
+        dist[1] = 0;
+
+        while (!pq.empty()) {
+            auto [d, node] = pq.top(); pq.pop();
+            if (d > dist[node]) continue;
+            for (auto& [nnode, w] : adj[node]) {
+                if (dist[node] + w < dist[nnode]) {
+                    dist[nnode] = dist[node] + w;
+                    pq.push({dist[nnode], nnode});
+                }
             }
-            else  ans.push_back(sum[query[1]]+get(1,0,2*n,st[query[1]]));
+        }
+
+        vector<int> euler;
+        dfs(1, -1, adj, euler);
+
+        int k = euler.size();
+        vector<long long> v(k);
+        map<int, int> st, ed;
+
+        for (int i = 0; i < k; i++) {
+            if (!st.count(euler[i])) st[euler[i]] = i;
+            v[i] = dist[euler[i]];
+        }
+        for (int i = k - 1; i >= 0; i--) {
+            if (!ed.count(euler[i])) ed[euler[i]] = i;
+        }
+
+        ST seg(k);
+        seg.build(0, 0, k - 1, v);
+
+        vector<int> ans;
+        for (auto& it : queries) {
+            if (it[0] == 1) {
+                int u = it[1], v = it[2], nwt = it[3];
+                int old = mp[{u, v}];
+                long long change = 1LL * nwt - old;
+
+                int start = (st[u] > st[v]) ? st[u] : st[v];
+                int end = (st[u] > st[v]) ? ed[u] : ed[v];
+
+                seg.update(0, 0, k - 1, start, end, change);
+                mp[{u, v}] = mp[{v, u}] = nwt;
+            } else {
+                int u = it[1];
+                ans.push_back(seg.query(0, 0, k - 1, st[u], st[u]));
+            }
         }
         return ans;
     }
