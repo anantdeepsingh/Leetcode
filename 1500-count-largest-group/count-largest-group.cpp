d27966e83fcdffd5187c842c437a6af52c1fff84
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-04-23 14:37:27 +0530

    Time: 227 ms (6.10%) | Memory: 7.9 MB (91.19%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int sumOfDigits(int num){
+        int sum=0;
+        while(num>0){
+            int rem=num%10;
+            sum+=rem;
+            num=num/10;
+        }
+        return sum;
+    }
+    int countLargestGroup(int n) {
+        vector<int>ans(100,0);
+        for(int i=1;i<=n;i++){
+            int sum=sumOfDigits(i);
+            cout<<sum<<endl;
+            ans[sum]++;
+        }
+        int largest_size=*max_element(ans.begin(),ans.end());
+        int cnt=0;
+        for(int i=1;i<30;i++){
+            if(ans[i]==largest_size){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
