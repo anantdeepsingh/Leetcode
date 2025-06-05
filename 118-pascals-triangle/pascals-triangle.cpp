b6a5183c349f4e41d9eb06891a25452237138561
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-06-05 19:18:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (32.12%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>ans(numRows,vector<int>(numRows,0));
-        ans[0][0]=1;
+        vector<vector<int>>ans(numRows);
+        ans[0].push_back(1);
         for(int i=1;i<numRows;i++){
             for(int j=0;j<=i;j++){
                 if(j==0 || j==i){
-                    ans[i][j]=1;
+                    ans[i].push_back(1);
                 }
                 else{
-                    ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
+                    ans[i].push_back(ans[i-1][j]+ans[i-1][j-1]);
                 }
             }
         }
-        vector<vector<int>>res(numRows);
-        for(int i=0;i<numRows;i++){
-            for(int j=0;j<=i;j++){
-                res[i].push_back(ans[i][j]);
-            }
-        }
-        return res;
+        
+        return ans;
     }
 };
