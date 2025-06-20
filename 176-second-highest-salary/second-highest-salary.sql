6169361e29c1e7e28affb14f30fddccaace10790
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-06-20 10:04:35 +0530

    Time: 246 ms (96.70%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,5 +1,5 @@
-select max(p1.salary) as SecondHighestSalary
-from Employee p1
-join Employee p2
-on p1.salary<p2.salary
--- join helps in putting null value by it self no need to put it manually 
+select
+(select distinct Salary 
+from Employee order by salary desc 
+limit 1 offset 1) 
+as SecondHighestSalary;
