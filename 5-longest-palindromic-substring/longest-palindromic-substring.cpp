e40ae121fe47a1524c95f52b7bb81c03c1edd22e
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-06-24 15:13:18 +0530

    Time: 1228 ms (5.08%) | Memory: 15.4 MB (51.77%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int n, maxLenAns = 0, startInd = 0;
+    vector<vector<int>> dp;
 
     bool check(const string &s, int i, int j){
         while(i < j){
@@ -10,25 +11,33 @@ public:
         return true;
     }
 
-    void findMaxLen(int ind, int cnt, string &s){
-        if(cnt == 0 || ind == n) return;
+    int findMaxLen(int ind, int cnt, string &s){
+        if (cnt == 0 || ind == n) return 0;
+        if (dp[ind][cnt] != -1) return dp[ind][cnt];
 
-        for(int j = ind; j < n; j++){
-            if(check(s, ind, j)){
+        int op1 = findMaxLen(ind + 1, cnt, s);  // skip current index
+        int op2 = 0;
+
+        for (int j = ind; j < n; j++) {
+            if (check(s, ind, j)) {
                 int len = j - ind + 1;
-                if(len > maxLenAns){
-                    maxLenAns = len;
-                    startInd = ind;
+                int total = len + findMaxLen(j + 1, cnt - 1, s);
+                if (total > op2) {
+                    op2 = total;
+                    if (cnt == 1 && len > maxLenAns) {  // we only want 1 palindrome
+                        maxLenAns = len;
+                        startInd = ind;
+                    }
                 }
-                findMaxLen(j + 1, cnt - 1, s);
             }
         }
 
-        findMaxLen(ind + 1, cnt, s);
+        return dp[ind][cnt] = max(op1, op2);
     }
 
     string longestPalindrome(string s) {
         n = s.size();
+        dp.assign(n + 1, vector<int>(2, -1));  // cnt only goes up to 1
         findMaxLen(0, 1, s);
         return s.substr(startInd, maxLenAns);
     }
