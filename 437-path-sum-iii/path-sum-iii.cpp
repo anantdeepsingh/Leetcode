221329b046df32ab8560fded9e9209a4e94aa024
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-06-25 16:43:26 +0530

    Time: 1305 ms (5.00%) | Memory: 376 MB (5.21%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ans = 0;
+
+    void dfs(TreeNode* root, long long sum, int targetSum, map<long long, int> mp) {
+        if (!root) return;
+
+        sum += root->val;
+        if (sum == targetSum) ans++;
+        ans += mp[sum - targetSum];
+
+        mp[sum]++; // add current prefix sum
+
+        dfs(root->left, sum, targetSum, mp);
+        dfs(root->right, sum, targetSum, mp);
+
+        mp[sum]--; // backtrack
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        map<long long, int> mp;
+        dfs(root, 0, targetSum, mp);
+        return ans;
+    }
+};
