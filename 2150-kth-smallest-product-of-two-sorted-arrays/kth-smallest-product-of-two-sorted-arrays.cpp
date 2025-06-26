af2f9084be2eb9c9990a0056c77f62771b154d2d
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-06-26 08:27:44 +0530

    Time: 1724 ms (5.17%) | Memory: 97.9 MB (98.56%) - LeetSync

--- 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.cpp
+++ 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.cpp
@@ -1,40 +1,63 @@
+#define ll long long
 class Solution {
 public:
-    int f(vector<int>& nums2, long long x1, long long v) {
-        int n2 = nums2.size();
-        int left = 0, right = n2 - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (x1 >= 0 && nums2[mid] * x1 <= v ||
-                x1 < 0 && nums2[mid] * x1 > v) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        if (x1 >= 0) {
-            return left;
-        } else {
-            return n2 - left;
-        }
-    }
+    ll cnt(vector<int>&nums1,vector<int>&nums2,ll k,ll pro){
+        long long productsCount = 0;
 
-    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2,
-                                 long long k) {
-        int n1 = nums1.size();
-        long long left = -1e10, right = 1e10;
-        while (left <= right) {
-            long long mid = (left + right) / 2;
-            long long count = 0;
-            for (int i = 0; i < n1; i++) {
-                count += f(nums2, nums1[i], mid);
-            }
-            if (count < k) {
-                left = mid + 1;
+        int n = nums2.size();
+
+        for(int i = 0; i < nums1.size(); i++) {
+            if(nums1[i] >= 0) {
+                int l = 0;
+                int r = n-1;
+                int m = -1; 
+                while(l <= r) {
+                    int mid = l + (r-l)/2;
+                    long long product = 1LL * nums1[i] * nums2[mid];
+                    if(product <= pro) {
+                        m = mid;
+                        l = mid+1;
+                    } else {
+                        r = mid-1;
+                    }
+                }
+                productsCount += (m+1); 
             } else {
-                right = mid - 1;
+                int l = 0;
+                int r = n-1;
+                int m = n; 
+                while(l <= r) {
+                    int mid = l + (r-l)/2;
+                    long long product = 1LL * nums1[i] * nums2[mid];
+
+                    if(product <= pro) {
+                        m = mid;
+                        r = mid-1;
+                    } else {
+                        l = mid+1;
+                    }
+                }
+                                                    
+                productsCount += (n - m);
             }
         }
-        return left;
+        return productsCount;
+    }
+    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
+        ll low=-1ll*1e10,high=1ll*1e10;
+        ll ans=0ll;
+        while(low<=high){
+            ll mid=low+(high-low)/2;
+            cout<<mid<<endl;
+            cout<<cnt(nums1,nums2,k,mid)<<endl;
+            if(cnt(nums1,nums2,k,mid)>=k){
+                ans=mid;
+                high=mid-1;
+            }
+            else if(cnt(nums1,nums2,k,mid)<k){
+                low=mid+1;
+            }
+        }
+        return ans;
     }
 };
