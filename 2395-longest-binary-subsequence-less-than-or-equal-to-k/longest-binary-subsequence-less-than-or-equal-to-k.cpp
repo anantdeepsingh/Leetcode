d53a6a6fef1862dadc8e49157a5c8e9864cc6bad
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-06-27 00:01:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (50.79%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -11,7 +11,7 @@ public:
         int ans=cnt;
         int ind=0;
         long long val=0;
-        for(int i=s.size()-1;i>=0 && ind<62;i--){
+        for(int i=s.size()-1;i>=0 && ind<64;i--){
             if(s[i]=='1'){
                 val=val|(1ll<<ind);
                 if(val<=k){
