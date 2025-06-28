4ccd6e2243e21b5ea35cc23facf7143c90175969
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-06-28 23:21:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (5.81%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -12,6 +12,18 @@ public:
     int rob(vector<int>& nums) {
         n=nums.size();
         dp.assign(n,-1);
-        return f(0,nums);
+        // return f(0,nums);
+        int ans=0;
+        dp[0]=nums[0];
+        ans=max(ans,dp[0]);
+        if(n>1){
+            dp[1]=max(nums[0],nums[1]);
+            ans=max(ans,dp[1]);
+        }
+        for(int i=2;i<n;i++){
+           dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+            ans=max(ans,dp[i]);
+        }
+        return ans;
     }
 };
