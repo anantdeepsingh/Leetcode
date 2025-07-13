b2ca623edc95f2b9a7b20e9a093c0a72c406a34e
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-07-13 13:49:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (77.72%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int number=0;
+        int sign=1;
+        int res=0;
+        stack<int>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==32) continue;
+            if(s[i]>='0' && s[i]<='9'){
+                number=number*10+(s[i]-'0');
+            }
+            else if(s[i]=='('){
+                st.push(res);
+                // cout<<sign<<endl;
+                st.push(sign);
+                number=0;
+                res=0;
+                sign=1;
+            }
+            else if(s[i]==')'){
+                res+=sign*number;
+                if(!st.empty()){
+                    int sg=st.top();
+                    st.pop();
+                    int val=st.top();
+                    st.pop();
+                    res=val+sg*res;
+                }
+                number=0;
+                sign=1;
+            }
+            else{
+                res+=sign*number;
+                if(s[i]=='-'){
+                    sign=-1;
+                }
+                else{
+                    sign=1;
+                }
+                number=0;
+            }
+
+            // cout<<res<<endl;
+        }
+        res+=sign*number;
+        return res;
+    }
+};
+
