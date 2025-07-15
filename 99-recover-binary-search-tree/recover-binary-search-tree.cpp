6ec3af5b7350b03bf82eb24d628ea6f3060b2d68
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-07-15 13:10:16 +0530

    Time: 0 ms (100.00%) | Memory: 60.7 MB (82.33%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    TreeNode* first = NULL;
+    TreeNode* middle = NULL;
+    TreeNode* last = NULL;
+    TreeNode* prev = NULL;
+
+    void traverse(TreeNode* root) {
+        if (root == NULL) return;
+
+        traverse(root->left);
+
+        if (prev && root->val < prev->val) {
+            if (!first) {
+                first = prev;
+                middle = root;
+            } else {
+                last = root;
+            }
+        }
+        prev = root;
+
+        traverse(root->right);
+    }
+
+    void recoverTree(TreeNode* root) {
+        first = middle = last = prev = NULL;
+        traverse(root);
+
+        if (last) {
+            swap(first->val, last->val);
+        } else {
+            swap(first->val, middle->val);
+        }
+    }
+};
