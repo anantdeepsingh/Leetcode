2a5081097b31d0bb2ba6b5ce744d54a9cb43d8f7
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-07-17 13:21:20 +0530

    Time: 28 ms (59.85%) | Memory: 15.5 MB (56.06%) - LeetSync

---
+++ 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -0,0 +1,39 @@
+#define ll long long
+class Solution {
+public:
+    long long validSubstringCount(string word1, string word2) {
+          unordered_map<char,int>mp;
+        for(auto &it:word2){
+            mp[it]++;
+        }
+
+        int cnt=0,l=0,r=0;
+        int n=word1.size();
+        int m=word2.size();
+        ll ans=0ll;
+        while(r<n){
+            if(mp[word1[r]]>0){
+                cnt++;
+            }
+            mp[word1[r]]--;
+            bool flag=false;
+            ll left=0ll;
+            while(cnt==m){
+                left++;
+                flag=true;
+                mp[word1[l]]++;
+                if(mp[word1[l]]>0){
+                    cnt--;
+                }
+                l++;
+            }
+
+            if(flag){
+                ans+=1ll*(left-1)*1ll*(n-1-r)+1ll*left-1ll+1ll*(n-1-r)+1ll;
+            }
+            r++;
+        }
+
+        return ans;
+    }
+};
