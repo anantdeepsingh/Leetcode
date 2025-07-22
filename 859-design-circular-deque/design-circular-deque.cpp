413508003acc5829abb1f93f080f0c98b55961b8
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-07-22 12:02:14 +0530

    Time: 2 ms (69.51%) | Memory: 24.6 MB (36.55%) - LeetSync

--- 859-design-circular-deque/design-circular-deque.cpp
+++ 859-design-circular-deque/design-circular-deque.cpp
@@ -1,81 +1,54 @@
+#include <list>
+using namespace std;
+
 class MyCircularDeque {
 public:
-    int k;
-    deque<int>dq;
+    list<int> dq;  // Standard library doubly linked list
+    int capacity;
+
     MyCircularDeque(int k) {
-       this->k=k; 
+        capacity = k;
     }
-    
+
     bool insertFront(int value) {
-        int sz=dq.size();
-        if(sz<k){
-            dq.push_front(value);
-            return true;
-        }
-        return false;
+        if (dq.size() == capacity) return false;
+        dq.push_front(value);
+        return true;
     }
-    
+
     bool insertLast(int value) {
-        int sz=dq.size();
-        if(sz<k){
-            dq.push_back(value);
-            return true;
-        }
-        return false;
+        if (dq.size() == capacity) return false;
+        dq.push_back(value);
+        return true;
     }
-    
+
     bool deleteFront() {
-        int sz=dq.size();
-        if(sz>0){
-            dq.pop_front();
-            return true;
-        }
-        return false;
+        if (dq.empty()) return false;
+        dq.pop_front();
+        return true;
     }
-    
+
     bool deleteLast() {
-        int sz=dq.size();
-        if(sz>0){
-            dq.pop_back();
-            return true;
-        }
-        return false;
+        if (dq.empty()) return false;
+        dq.pop_back();
+        return true;
     }
-    
+
     int getFront() {
-        int sz=dq.size();
-        if(sz>0){
-           return dq.front();
-        }
-        return -1;
+        if (dq.empty()) return -1;
+        return dq.front();
     }
-    
+
     int getRear() {
-        int sz=dq.size();
-        if(sz>0){
-           return dq.back();
-        }
-        return -1;
+        if (dq.empty()) return -1;
+        return dq.back();
     }
-    
+
     bool isEmpty() {
-        return dq.size()==0;
+        return dq.empty();
     }
-    
+
     bool isFull() {
-        return dq.size()==k;
+        return dq.size() == capacity;
     }
 };
-
-/**
- * Your MyCircularDeque object will be instantiated and called as such:
- * MyCircularDeque* obj = new MyCircularDeque(k);
- * bool param_1 = obj->insertFront(value);
- * bool param_2 = obj->insertLast(value);
- * bool param_3 = obj->deleteFront();
- * bool param_4 = obj->deleteLast();
- * int param_5 = obj->getFront();
- * int param_6 = obj->getRear();
- * bool param_7 = obj->isEmpty();
- * bool param_8 = obj->isFull();
- */
