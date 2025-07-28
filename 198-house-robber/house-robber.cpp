60aa1903642e8833efce1060b8e29b09f63f0d70
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-07-28 13:29:41 +0530

    Time: 1 ms (9.36%) | Memory: 10.7 MB (22.59%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    int n;
-    vector<int>dp;
-    int f(int i,vector<int>&nums){
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int op1=f(i+1,nums);
-        int op2=nums[i]+f(i+2,nums);
-        return dp[i]=max(op1,op2);
+    int maxAmount(int ind,vector<int>&nums,vector<int>&dp){
+        int n=nums.size();
+        if(ind>=n) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int op1=maxAmount(ind+1,nums,dp);
+        int op2=nums[ind]+maxAmount(ind+2,nums,dp);
+        return dp[ind]=max(op1,op2);
     }
     int rob(vector<int>& nums) {
-        n=nums.size();
-        dp.assign(n,-1);
-        // return f(0,nums);
-        int ans=0;
-        dp[0]=nums[0];
-        ans=max(ans,dp[0]);
-        if(n>1){
-            dp[1]=max(nums[0],nums[1]);
-            ans=max(ans,dp[1]);
-        }
-        for(int i=2;i<n;i++){
-           dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
-            ans=max(ans,dp[i]);
-        }
-        return ans;
+        vector<int>dp(nums.size(),-1);
+        return maxAmount(0,nums,dp);
     }
 };
