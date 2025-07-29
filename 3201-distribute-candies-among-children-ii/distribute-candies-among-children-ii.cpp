40add0ed3c8a32eb315e47087c8bcb6822c8ceca
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-07-29 16:06:50 +0530

    Time: 128 ms (7.09%) | Memory: 315.3 MB (17.45%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     long long distributeCandies(int n, int limit) {
-        long long ans = 0;
-        for (int i = 0; i <= min(limit, n); i++) {
-            if (n - i > 2 * limit) {
-                continue;
-            }
-            ans += min(n - i, limit) - max(0, n - i - limit) + 1;
+        vector<long long>count(2*limit+1,0ll);
+        for(int i=0;i<=limit;i++){
+            count[i]=1ll*i+1ll*1;
         }
+        int k=limit;
+        for(int i=limit+1;i<=2*limit;i++){
+            count[i]=k;
+            k--;
+        }
+
+
+        long long ans=0;
+
+        for(int i=0;i<=limit;i++){
+            long long left=1ll*n-1ll*i;
+            if(left>2*limit) continue;
+            if(left>=0) ans+=1ll*count[n-i];
+        }
+
         return ans;
     }
 };
