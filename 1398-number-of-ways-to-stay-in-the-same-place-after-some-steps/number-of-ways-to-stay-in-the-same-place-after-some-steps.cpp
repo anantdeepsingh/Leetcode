f32c2b98e406fcc04c4ecf0790f699792cbdf5f6
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-08-04 16:05:25 +0530

    Time: 11 ms (64.53%) | Memory: 9 MB (83.36%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -3,8 +3,8 @@ class Solution {
 public:
     int numWays(int steps, int arrLen) {
         int n = min(arrLen, steps + 1);
-        vector<vector<long long>>dp(n+1,vector<long long>(steps+1,0));
-        dp[0][0]=1ll;
+        vector<long long>prev(steps+1,0),curr(steps+1,0);
+        prev[0]=1;
 
 
 
@@ -12,14 +12,29 @@ public:
         for(int s=1;s<=steps;s++){
             for(int i=n-1;i>=0;i--){
                 long long op1=0,op2=0,op3=0;
-                if(i+1<=n-1) op1=dp[i+1][s-1]%mod;
-                if(i<=n-1) op2=dp[i][s-1]%mod;
-                if(i-1>=0) op3=dp[i-1][s-1]%mod;
-                dp[i][s]=(1ll*op1%mod+op2%mod+op3%mod)%mod;
+                if(i+1<=n-1) op1=prev[i+1]%mod;
+                if(i<=n-1) op2=prev[i]%mod;
+                if(i-1>=0) op3=prev[i-1]%mod;
+                curr[i]=(1ll*op1%mod+op2%mod+op3%mod)%mod;
             }    
+            prev=curr;
         }
        
 
-        return dp[0][steps];
+        return prev[0];
     }
 };
+
+
+/*
+
+dp[i][s] => dp[i-1][s-1]
+dp[i][s]=>dp[i+1][s+1]
+
+*** * ***
+*** * ***
+*** * ***
+
+
+
+*/
