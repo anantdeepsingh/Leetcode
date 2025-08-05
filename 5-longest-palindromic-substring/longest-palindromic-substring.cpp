2c8ad80c33645fadb0178595a7a737a3e6695854
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-08-05 18:48:23 +0530

    Time: 858 ms (9.93%) | Memory: 9.5 MB (68.40%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
-    int n, maxLenAns = 0, startInd = 0;
-    vector<vector<int>> dp;
-
-    bool check(const string &s, int i, int j){
-        while(i < j){
-            if(s[i] != s[j]) return false;
-            i++; j--;
+    bool solve(string &s,int  i,int j){
+        if(i>=j) return true;
+        if(s[i]==s[j]){
+            return solve(s,i+1,j-1);
         }
-        return true;
+        return false;
     }
-
-    int findMaxLen(int ind, int cnt, string &s){
-        if (cnt == 0 || ind == n) return 0;
-        if (dp[ind][cnt] != -1) return dp[ind][cnt];
-
-        int op1 = findMaxLen(ind + 1, cnt, s);  // skip current index
-        int op2 = 0;
-
-        for (int j = ind; j < n; j++) {
-            if (check(s, ind, j)) {
-                int len = j - ind + 1;
-                int total = len + findMaxLen(j + 1, cnt - 1, s);
-                if (total > op2) {
-                    op2 = total;
-                    if (cnt == 1 && len > maxLenAns) {  // we only want 1 palindrome
-                        maxLenAns = len;
-                        startInd = ind;
+    string longestPalindrome(string s) {
+        int n=s.size();
+        int maxLen=0;
+        int sInd=-1;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(solve(s,i,j)){
+                    if(j-i+1>maxLen){
+                        maxLen=j-i+1;
+                        sInd=i;
                     }
                 }
             }
         }
 
-        return dp[ind][cnt] = max(op1, op2);
-    }
-
-    string longestPalindrome(string s) {
-        n = s.size();
-        dp.assign(n + 1, vector<int>(2, -1));  // cnt only goes up to 1
-        findMaxLen(0, 1, s);
-        return s.substr(startInd, maxLenAns);
+        return s.substr(sInd,maxLen);
     }
 };
