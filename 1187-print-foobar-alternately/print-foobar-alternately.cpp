51b3e7acffd360b77920c0493fa3d45c188e883a
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-08-07 23:44:00 +0530

    Time: 9 ms (48.13%) | Memory: 11.3 MB (75.02%) - LeetSync

---
+++ 1187-print-foobar-alternately/print-foobar-alternately.cpp
@@ -0,0 +1,41 @@
+class FooBar {
+private:
+    int n;
+    mutex m;
+    int turn;
+    condition_variable cv;
+public:
+    FooBar(int n) {
+        this->n = n;
+    }
+
+    void foo(function<void()> printFoo) {
+        
+        for (int i = 0; i < n; i++) {
+            
+        	// printFoo() outputs "foo". Do not change or remove this line.
+            unique_lock<mutex>lock(m);
+            while(turn!=0){
+                cv.wait(lock);
+            }
+        	printFoo();
+            turn=1;
+            cv.notify_all();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        
+        for (int i = 0; i < n; i++) {
+            
+        	// printBar() outputs "bar". Do not change or remove this line.
+            unique_lock<mutex>lock(m);
+            while(turn!=1){
+                cv.wait(lock);
+            }
+        	printBar();
+            turn=0;
+            cv.notify_all();
+        }
+    }
+};
