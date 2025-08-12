057461c72d947c2aa472d66e4727f2604add0471
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-08-12 16:04:17 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (28.90%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,34 +11,45 @@
  */
 class Solution {
 public:
+    bool Check(TreeNode* p,TreeNode* q){
+        if(p==NULL || q==NULL) return p==q;
+        if(p->val!=q->val) return false;
 
+        return Check(p->left,q->right) && Check(p->right,q->left);
+    }
     bool isSymmetric(TreeNode* root) {
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            vector<string>v;
-            int sz=q.size();
-            for(int i=0;i<sz;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                if(node){
-                    v.push_back(to_string(node->val));
-                    q.push(node->left);
-                    q.push(node->right);
-                }
-                else{
-                    v.push_back("*");
-                }
-            }
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     vector<string>v;
+        //     int sz=q.size();
+        //     for(int i=0;i<sz;i++){
+        //         TreeNode* node=q.front();
+        //         q.pop();
+        //         if(node){
+        //             v.push_back(to_string(node->val));
+        //             q.push(node->left);
+        //             q.push(node->right);
+        //         }
+        //         else{
+        //             v.push_back("*");
+        //         }
+        //     }
 
-            int l=0,r=v.size()-1;
-            while(l<r){
-                if(v[l]!=v[r]) return false;
-                l++;
-                r--;
-            }
-        }
+        //     int l=0,r=v.size()-1;
+        //     while(l<r){
+        //         if(v[l]!=v[r]) return false;
+        //         l++;
+        //         r--;
+        //     }
+        // }
 
-        return true;
+        // return true;
+
+
+        if(root==NULL) return true;
+
+        return Check(root->left,root->right);
+
     }
 };
