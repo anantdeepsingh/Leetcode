e114a7a64ef58380644f1bb88233a4da1b64410b
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-08-14 15:22:37 +0530

    Time: 7 ms (59.58%) | Memory: 14.3 MB (19.54%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
-    int dp[501][501];
-    int f(int i,int j,string s1,string s2){
-        if(j<0){
-            return i+1;
-        }
-        if(i<0){
-            return j+1;
-        }
+    vector<vector<int>>dp;
+    int f(int i,int j,string &s1,string &s2){
+        if(i==0 && j==0) return 0;
+        if(i==0) return j;
+        if(j==0) return i;
         if(dp[i][j]!=-1) return dp[i][j];
-        int op1=1e5,op2=1e5,op3=1e5;
-        // op1 insert
-        // op2 delete
-        // op3 replace
-        if(s1[i]!=s2[j]){
-            op1=min(op1,1+f(i,j-1,s1,s2));
-            op2=min(op2,1+f(i-1,j,s1,s2));
-            op3=min(op3,1+f(i-1,j-1,s1,s2));
+        int ans=INT_MAX;
+        if(s1[i-1]==s2[j-1]){
+            ans=min(ans,f(i-1,j-1,s1,s2));
         }
         else{
-           return dp[i][j]=f(i-1,j-1,s1,s2);
+            int op1=1+f(i,j-1,s1,s2);
+            int op2=1+f(i-1,j-1,s1,s2);
+            int op3=1+f(i-1,j,s1,s2);
+            ans=min(ans,min({op1,op2,op3}));
         }
 
-        return dp[i][j]=min({op1,op2,op3});
+        return dp[i][j]=ans;
     }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        memset(dp,-1,sizeof(dp));
-        return f(n-1,m-1,word1,word2);
+        dp.assign(n+1,vector<int>(m+1,-1));
+        return f(n,m,word1,word2);
     }
 };
+
+
+
+// HORSE
+//   ROS
+
+//   F(4,2)
+//   F(4,1),F(3,2),F(3,3)
+
+//   IF(I<0 && J<0) RETURN 0;
+//   IF(I<0) RETURN J+1; INSERT
+//   IF(J<0) RETURN I+1 DELETE
+
+
+
+// F(0,1)
