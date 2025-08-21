cce167714ca44bc80722b81503bb43c8806fa35a
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-08-21 09:48:20 +0530

    Time: 9 ms (63.20%) | Memory: 22.3 MB (10.41%) - LeetSync

---
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int numSubmat(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            vector<int>sum(m,0);
+            for(int j=0;j<m;j++){
+                if(i!=0){
+                    if(mat[i][j]==0) continue;
+                    mat[i][j]=mat[i-1][j]+1;
+                }
+            }
+
+            stack<int>st;
+            vector<int>left(m);
+            for(int j=0;j<m;j++){
+                if(st.empty()){
+                    left[j]=-1;
+                }
+                while(!st.empty() && mat[i][j]<=mat[i][st.top()]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    left[j]=-1;
+                }
+                else{
+                    left[j]=st.top();
+                }
+                st.push(j);
+            }
+
+
+            for(int j=0;j<m;j++){
+                int ind=left[j];
+                if(ind==-1){
+                    sum[j]+=(j+1)*mat[i][j];
+                }
+                else{
+                    sum[j]=sum[ind]+(j-ind)*mat[i][j];
+                }
+            }
+
+            for(int j=0;j<m;j++){
+                ans+=sum[j];
+            }
+        }
+        return ans;
+    }
+};
+
+
+
