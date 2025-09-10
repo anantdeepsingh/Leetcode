287ff8b7e0f00bc07cefb774c0da3601b683350a
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-09-10 12:01:22 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (79.67%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,83 +1,34 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        if (n == 0) return 0;
-
-        // prefix sum
-        vector<int> pre(n, 0);
-        pre[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] + height[i];
+        int n=height.size();
+        int mini=height[0];
+        int sum=height[0];
+        int ans=0;
+        int prev=0;
+        for(int i=1;i<n;i++){
+            if(height[i]>=mini){
+                ans+=(i-prev)*mini-sum;
+                sum=0;
+                prev=i;
+                mini=height[i];
+            }
+            sum+=height[i];
         }
-
-        // next greater (to the right)
-        stack<int> st;
-        vector<int> next_greater(n);
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && height[i] > height[st.top()]) {
-                st.pop();
+        // cout<<ans<<endl;
+        mini=height[n-1];
+        sum=height[n-1];
+        prev=n-1;
+        for(int i=n-2;i>=0;i--){
+            if(height[i]>mini){
+                // cout<<(prev-i)*mini-sum<<endl;
+                ans+=(prev-i)*mini-sum;
+                sum=0;
+                prev=i;
+                mini=height[i];
             }
-            next_greater[i] = st.empty() ? n : st.top();
-            st.push(i);
+            sum+=height[i];
         }
-
-        // prev greater (to the left)
-        while (!st.empty()) st.pop();
-        vector<int> prev_greater(n);
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && height[i] > height[st.top()]) {
-                st.pop();
-            }
-            prev_greater[i] = st.empty() ? -1 : st.top();
-            st.push(i);
-        }
-
-        long long ans = 0;
-
-        // forward traversal until highest peak
-        int r = 0;
-        int maxIdx = 0;
-        for (int i = 1; i < n; i++) {
-            if (height[i] > height[maxIdx]) {
-                maxIdx = i; // tallest bar index
-            }
-        }
-
-        while (r < maxIdx) {
-            if (height[r] == 0 || next_greater[r] == n) {
-                r++;
-                continue;
-            }
-            int ind = next_greater[r];
-            if (ind > maxIdx) ind = maxIdx; // don’t cross tallest bar
-
-            ans += 1LL * height[r] * (ind - r - 1);
-            if (r + 1 <= ind - 1) {
-                ans -= pre[ind - 1] - pre[r];
-            }
-
-            r = ind;
-        }
-
-        // backward traversal from right until tallest peak
-        int l = n - 1;
-        while (l > maxIdx) {
-            if (height[l] == 0 || prev_greater[l] == -1) {
-                l--;
-                continue;
-            }
-            int ind = prev_greater[l];
-            if (ind < maxIdx) ind = maxIdx; // don’t cross tallest bar
-
-            ans += 1LL * height[l] * (l - ind - 1);
-            if (ind + 1 <= l - 1) {
-                ans -= pre[l - 1] - (ind >= 0 ? pre[ind] : 0);
-            }
-
-            l = ind;
-        }
-
-        return (int)ans;
+        return ans;
     }
 };
