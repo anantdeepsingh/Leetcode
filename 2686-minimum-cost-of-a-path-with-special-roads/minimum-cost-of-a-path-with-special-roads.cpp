eaf1bdc7a573a3064a89c6dbad262e0dfa3a7c9a
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-09-14 15:14:23 +0530

    Time: 189 ms (23.03%) | Memory: 127.8 MB (27.68%) - LeetSync

--- 2686-minimum-cost-of-a-path-with-special-roads/minimum-cost-of-a-path-with-special-roads.cpp
+++ 2686-minimum-cost-of-a-path-with-special-roads/minimum-cost-of-a-path-with-special-roads.cpp
@@ -2,85 +2,93 @@
 class Solution {
 public:
     int minimumCost(vector<int>& start, vector<int>& target, vector<vector<int>>& specialRoads) {
-        int n = specialRoads.size();
-        map<pair<int,int>,int> mp1;  // coordinate -> node index
-        map<int,pair<int,int>> mp2;  // node index -> coordinate
-        int c = 0;
-
-        // add start
-        if(mp1.count({start[0], start[1]}) == 0){
-            mp2[c] = {start[0], start[1]};
-            mp1[{start[0], start[1]}] = c;
+        // priority_queue<pi,vector<pi>,greater<pi>>pq;
+        int n=specialRoads.size();
+        map<pair<int,int>,int>mp1;
+        map<int,pair<int,int>>mp2;
+        int c=0;
+        if(mp1.count({start[0],start[1]})==0){
+            mp2[c]={start[0],start[1]};
+            mp1[{start[0],start[1]}]=c;
             c++;
         }
-
-        // add special road endpoints
         for(int i=0;i<n;i++){
+            int u=specialRoads[i][0];
+            int v=specialRoads[i][1];
+            int x=specialRoads[i][2];
+            int y=specialRoads[i][3];
+
+            if(mp1.count({u,v})==0){
+                mp1[{u,v}]=c;
+                mp2[c]={u,v};
+                c++;
+            }
+
+            if(mp1.count({x,y})==0){
+                mp1[{x,y}]=c;
+                mp2[c]={x,y};
+                c++;
+            }
+
+
+        }
+
+        if(mp1.count({target[0],target[1]})==0){
+            mp2[c]={target[0],target[1]};
+            mp1[{target[0],target[1]}]=c;
+            c++;
+        }
+        vector<vector<pair<int,int>>>adj(c);
+        for(int i=0;i<c;i++){
+            for(int j=i+1;j<c;j++){
+                adj[i].push_back({j,(abs(mp2[i].first-mp2[j].first)+abs(mp2[i].second-mp2[j].second))});
+                adj[j].push_back({i,(abs(mp2[i].first-mp2[j].first)+abs(mp2[i].second-mp2[j].second))});
+            }
+        }
+
+       for(int i=0;i<n;i++){
             int u = specialRoads[i][0];
             int v = specialRoads[i][1];
             int x = specialRoads[i][2];
             int y = specialRoads[i][3];
-
-            if(mp1.count({u,v})==0){
-                mp1[{u,v}] = c;
-                mp2[c] = {u,v};
-                c++;
-            }
-            if(mp1.count({x,y})==0){
-                mp1[{x,y}] = c;
-                mp2[c] = {x,y};
-                c++;
-            }
-        }
-
-        // add target
-        if(mp1.count({target[0], target[1]})==0){
-            mp2[c] = {target[0], target[1]};
-            mp1[{target[0], target[1]}] = c;
-            c++;
-        }
-
-        // adjacency list
-        vector<vector<pair<int,int>>> adj(c);
-
-        // connect all nodes with Manhattan distance (both directions)
-        for(int i=0;i<c;i++){
-            for(int j=i+1;j<c;j++){
-                int dist = abs(mp2[i].first - mp2[j].first) + abs(mp2[i].second - mp2[j].second);
-                adj[i].push_back({j, dist});
-                adj[j].push_back({i, dist});
-            }
-        }
-
-        // add special roads (directed edges)
-        for(int i=0;i<n;i++){
-            int u = mp1[{specialRoads[i][0], specialRoads[i][1]}];
-            int v = mp1[{specialRoads[i][2], specialRoads[i][3]}];
             int wt = specialRoads[i][4];
-            adj[u].push_back({v, wt});
+
+            int node1 = mp1[{u,v}];
+            int node2 = mp1[{x,y}];
+
+            bool found = false;
+            // for(auto &it : adj[node1]){
+            //     if(it.first == node2){
+            //         it.second = min(it.second, wt);
+            //         found = true;
+            //         break;
+            //     }
+            // }
+            if(!found){
+                adj[node1].push_back({node2, wt}); 
+            }
         }
 
-        // Dijkstra
-        priority_queue<pi, vector<pi>, greater<pi>> pq;
-        vector<int> dist(c, 1e9);
-        dist[0] = 0;
-        pq.push({0, 0});
 
+        priority_queue<pi,vector<pi>,greater<pi>>pq;
+        pq.push({0,0});
+        vector<int>dist(c,1e8);
+        dist[0]=0;
         while(!pq.empty()){
-            auto [d, node] = pq.top();
+            int d=pq.top().first;
+            int node=pq.top().second;
             pq.pop();
-            if(d > dist[node]) continue; // skip stale entry
 
-            for(auto &it : adj[node]){
-                int adjNode = it.first;
-                int wt = it.second;
-                if(dist[adjNode] > d + wt){
-                    dist[adjNode] = d + wt;
-                    pq.push({dist[adjNode], adjNode});
+            for(auto &it:adj[node]){
+                int adjNode=it.first;
+                int wt=it.second;
+                if(dist[adjNode]>wt+d){
+                    dist[adjNode]=wt+d;
+                    pq.push({wt+d,adjNode});
                 }
             }
         }
 
-        return dist[mp1[{target[0], target[1]}]];
+        return dist[mp1[{target[0],target[1]}]];
     }
 };
