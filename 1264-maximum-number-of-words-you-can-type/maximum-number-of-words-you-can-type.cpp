1bd3bae431e770012068a9fcdf91b6a1bcd9a93d
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-09-15 10:28:18 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (57.81%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -2,28 +2,17 @@ class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
         vector<string>a;
-        string s="";
         int n=text.size();
-        for(int i=0;i<n;i++){
-            if(text[i]==32){
-                if(s.size()>0) a.push_back(s);
-                s="";
-            }
-            else{
-                s+=text[i];
-            }
-        }
-        if(s.size()>0){
-            a.push_back(s);
-        }
         map<char,int>mp;
         for(auto &it:brokenLetters){
             mp[it]++;
         }
+        stringstream ss(text);
+        string s;
         int cnt=0;
-        for(auto &it:a){
+        while(ss>>s){
             bool flag=true;
-            for(auto &i:it){
+            for(auto &i:s){
                 if(mp.count(i)){
                     flag=false;
                     break;
