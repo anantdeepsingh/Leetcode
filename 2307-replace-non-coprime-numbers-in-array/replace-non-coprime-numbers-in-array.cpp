ea09362d4f4a1fa52fcaf2bb8cb60ef854f3735e
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-09-17 11:46:56 +0530

    Time: 13 ms (88.02%) | Memory: 125.2 MB (44.70%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -1,82 +1,22 @@
 class Solution {
 public:
-    vector<int> replaceNonCoprimes(vector<int>& nums) {
-        // deque<int>dq;
-        // for(auto &it:nums){
-        //     dq.push_back(it);
-        // }
-        // vector<int>ans;
-        // while(dq.size()>1){
-        //     int top1=dq.front();
-        //     dq.pop_front();
-        //     int top2=dq.front();
-        //     dq.pop_front();
-        //     int hcf=__gcd(top1,top2);
-        //     if(hcf==1){
-        //         // cout<<top1<<endl;
-        //         ans.push_back(top1);
-        //         dq.push_front(top2);
-        //     }
-        //     else{
-        //         // cout<<top1<<" "<<top2<<endl;
-        //         long long lcm=(1ll*top1*top2)/hcf;
-        //         // cout<<lcm<<endl;
-        //         dq.push_front(lcm);
-        //     }
-        // }
-        // ans.push_back(dq.front());
+    static int lcm(int x, int y) {
+        return x/gcd(x, y)*y;
+    }
 
-
-        vector<int>res;
-        res.push_back(nums[0]);
-        for(int i=1;i<nums.size();i++){
-            int hcf=__gcd(nums[i],res.back());
-            cout<<hcf<<endl;
-            if(hcf==1){
-                int k=10;
-                while(k>1 && res.size()>1){
-                    int top1=res.back();
-                    res.pop_back();
-                    int top2=res.back();
-                    res.pop_back();
-                    k=__gcd(top1,top2);
-                    if(k==1){
-                        res.push_back(top2);
-                        res.push_back(top1);
-                        break;
-                    }
-                    else{
-                        long long lcm=(1ll*top1*top2)/k;
-                        res.push_back(lcm);
-                    }
-                }
-                res.push_back(nums[i]);
+    static vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int> st;
+        for (int x : nums) {
+            int cur=x;
+            while (st.size()) {
+                int g=gcd(st.back(), cur);
+                if (g==1) break;
+                cur=lcm(st.back(), cur);
+                st.pop_back();// pop 
             }
-            else{
-                long long lcm=(1ll*nums[i]*res.back())/hcf;
-                res.pop_back();
-                cout<<lcm<<endl;
-                res.push_back(lcm);
-
-            }
+            st.push_back(cur);
         }
-        int k=10;
-        while(k>1 && res.size()>1){
-            int top1=res.back();
-            res.pop_back();
-            int top2=res.back();
-            res.pop_back();
-            k=__gcd(top1,top2);
-            if(k==1){
-                res.push_back(top2);
-                res.push_back(top1);
-                break;
-            }
-            else{
-                long long lcm=(1ll*top1*top2)/k;
-                res.push_back(lcm);
-            }
-        }
-        return res;
+
+        return st;
     }
 };
